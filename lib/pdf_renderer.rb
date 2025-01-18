f67fed9beb852f716ba722a58c4ce07804dc5c54
denialtorres <denial.torres@gmail.com>
2025-01-17 23:48:51 -0600

    include custom render pdf logic

---
+++ lib/pdf_renderer.rb
@@ -0,0 +1,9 @@
+require "pdf_renderer/version"
+require "pdf_renderer/railtie"
+require "prawn"
+
+ActionController::Renderers.add :pdf do |filename, options|
+  pdf = Prawn::Document.new
+  pdf.text render_to_string(options)
+  send_data(pdf.render, filename: "#{filename}.pdf", disposition: "attachment")
+end
